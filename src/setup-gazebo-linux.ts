961cd9927a72cc089a2576aadfee995758fc5c9f
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-06-01 11:46:55 +0800

    Added os config setup for linux
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

---
+++ src/setup-gazebo-linux.ts
@@ -0,0 +1,58 @@
+import * as core from "@actions/core";
+import * as io from "@actions/io";
+
+import * as apt from "./package_manager/apt";
+import * as utils from "./utils";
+
+import * as path from "path";
+import fs from "fs";
+
+/**
+ * Configure basic OS stuff.
+ */
+async function configOs(): Promise<void> {
+	// When this action runs in a Docker image, sudo may be missing.
+	// This installs sudo to avoid having to handle both cases (action runs as
+	// root, action does not run as root) everywhere in the action.
+	try {
+		await io.which("sudo", true);
+	} catch (err) {
+		await utils.exec("apt-get", ["update"]);
+		await utils.exec("apt-get", [
+			"install",
+			"--no-install-recommends",
+			"--quiet",
+			"--yes",
+			"sudo",
+		]);
+	}
+
+	await utils.exec("sudo", ["bash", "-c", "echo 'Etc/UTC' > /etc/timezone"]);
+	await utils.exec("sudo", ["apt-get", "update"]);
+
+	// Install tools required to configure the worker system.
+	await apt.runAptGetInstall(["curl", "gnupg2", "locales", "lsb-release"]);
+
+	// Select a locale supporting Unicode.
+	await utils.exec("sudo", ["locale-gen", "en_US", "en_US.UTF-8"]);
+	core.exportVariable("LANG", "en_US.UTF-8");
+
+	// Enforce UTC time for consistency.
+	await utils.exec("sudo", ["bash", "-c", "echo 'Etc/UTC' > /etc/timezone"]);
+	await utils.exec("sudo", [
+		"ln",
+		"-sf",
+		"/usr/share/zoneinfo/Etc/UTC",
+		"/etc/localtime",
+	]);
+	await apt.runAptGetInstall(["tzdata"]);
+}
+
+/**
+ * Install Gazebo on a Linux worker.
+ */
+export async function runLinux(): Promise<void> {
+
+	await configOs();
+
+}
--- src/setup-gazebo.ts
+++ src/setup-gazebo.ts
@@ -1,1 +1,17 @@
-console.log("setup-gazebo")
+import * as core from "@actions/core";
+import * as linux from "./setup-gazebo-linux";
+
+async function run() {
+  try {
+    linux.runLinux();
+  }
+  catch (error) {
+    let errorMessage = "Unknown error";
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		}
+		core.setFailed(errorMessage);
+  }
+}
+
+run();
