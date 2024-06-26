44550535af2744a5d52a748a39dff690852bef7c
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-06-27 01:05:32 +0800

    Modified conda command for testing
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- src/setup-gazebo-windows.ts
+++ src/setup-gazebo-windows.ts
@@ -1,5 +1,12 @@
-import * as conda from "./package_manager/conda";
+import * as utils from "./utils";
 
 export async function runWindows(): Promise<void> {
-	await conda.runConda(["gz-sim8"]);
+	// await conda.runConda(["gz-sim8"]);
+	await utils.exec("conda", [
+		"search",
+		"libgz-sim*",
+		"--channel",
+		"conda-forge",
+	]);
+	await utils.exec("conda", ["install", "gz-sim8", "--channel", "conda-forge"]);
 }
