c0272ff1a691c93d526f710b7aa0e2b31b6dcb8f
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-06-27 00:08:30 +0800

    Added conda cmd to install gz-sim
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -26269,6 +26269,62 @@ exports.runBrew = runBrew;
 
 /***/ }),
 
+/***/ 7725:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.runConda = void 0;
+const utils = __importStar(__nccwpck_require__(1314));
+/**
+ * Run conda install on a list of specified packages.
+ *
+ * @param   packages list of conda-forge packages to be installed
+ * @returns Promise<number> exit code
+ */
+function runConda(packages) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return utils.exec("conda", ["install"].concat(packages).concat(["--channel conda-forge"]));
+    });
+}
+exports.runConda = runConda;
+
+
+/***/ }),
+
 /***/ 5467:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -26485,6 +26541,56 @@ exports.runMacOs = runMacOs;
 
 /***/ }),
 
+/***/ 8502:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.runWindows = void 0;
+const conda = __importStar(__nccwpck_require__(7725));
+function runWindows() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield conda.runConda(["gz-sim8"]);
+    });
+}
+exports.runWindows = runWindows;
+
+
+/***/ }),
+
 /***/ 525:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -26526,6 +26632,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const linux = __importStar(__nccwpck_require__(5467));
 const macOs = __importStar(__nccwpck_require__(8247));
+const windows = __importStar(__nccwpck_require__(8502));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -26535,6 +26642,9 @@ function run() {
             }
             else if (platform === "linux") {
                 linux.runLinux();
+            }
+            else if (platform === "win32") {
+                windows.runWindows();
             }
             else {
                 throw new Error(`Unsupported platform ${platform}`);
