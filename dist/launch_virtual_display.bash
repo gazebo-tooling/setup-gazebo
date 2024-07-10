c0e044d58b4a6188ce207a9cdd84ab2d26cd7abb
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-07-11 00:14:28 +0800

    Testing disown after command
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- dist/launch_virtual_display.bash
+++ dist/launch_virtual_display.bash
@@ -2,6 +2,6 @@
 
 set -x
 
-Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 > /dev/null 2>&1 &
+Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 & disown
 export DISPLAY=:1.0
 export MESA_GL_VERSION_OVERRIDE=3.3
