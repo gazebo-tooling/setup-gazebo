239b4966cf80ad729d0a85b3874c1d17e2ef7b28
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-07-11 00:55:00 +0800

    Echo PID at the end of xvfb launch
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- src/launch_virtual_display.bash
+++ src/launch_virtual_display.bash
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
-Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 > /dev/null 2>&1 &
+Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 > /dev/null 2>&1 & disown
 export DISPLAY=:1.0
 export MESA_GL_VERSION_OVERRIDE=3.3
+
+echo "$!"
