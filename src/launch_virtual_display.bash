86be9a5c6530e27f175b0c71cbe27d66a73e9046
Saurabh Kamat <kamatsaurabh01@gmail.com>
2024-07-11 00:41:14 +0800

    Check for PID after xvfb
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- src/launch_virtual_display.bash
+++ src/launch_virtual_display.bash
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-set -x
-
-Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 &! disown
+Xvfb :1 -ac -noreset -core -screen 0 1280x1024x24 > /dev/null 2>&1 &
 export DISPLAY=:1.0
 export MESA_GL_VERSION_OVERRIDE=3.3
+
+jobs
